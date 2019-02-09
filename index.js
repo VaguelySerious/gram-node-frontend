3595afa70ce23719ed9994dae5d0d854507013c6
Patricia Steves <pats@hotmail.de>
2019-02-09 18:07:52 +0100

    Add styles for chatbox

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import './styles/styles.scss';
+import './styles/chat.scss';
+import './styles/demo.scss';
 import GramChatTemplate from './components/gram.html'
 import uuidv4 from 'uuid/v4';
 
@@ -10,13 +11,13 @@ window.Gram = {
     try {
       user = localStorage.getItem('user');
       if (user) {
-        
+
         console.log('Remembered user', user);
       } else {
         user = uuidv4();
         localStorage.setItem('user', user);
         console.log('Created new user', user);
-      }  
+      }
     } catch (e) {
       user = uuidv4();
       console.log('Created temporary user (check if site allows localStorage)');
